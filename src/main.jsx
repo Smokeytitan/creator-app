970367883aaaa7179e33cd25a59b05f3d6c9a93a
smokeytitan <ntruslow@polygon.technology>
2026-01-07 02:15:04 -0800

    Completely remove ClerkProvider to fix loading issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext'
-import { ClerkProvider } from '@clerk/clerk-react'
-
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ClerkProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
